314f2aa67bc53ac10f4a4869c7a20ccd52cb52fe
Andres Giraldo Arenas <andresganc@gmail.com>
2022-12-14 12:21:23 -0500

    Mod: Exmaple 1

--- course-nc/process/process-argv/example1.js
+++ course-nc/process/process-argv/example1.js
@@ -2,14 +2,22 @@
 // Example 1
 // Function for control process
 
+// Use
+// $ node example1 --name "Andres" --lastname "Giraldo" --age "43"
+
 function param(p) {
     let index = process.argv.indexOf(p)
     //console.log(index)
     return process.argv[index + 1]
 }
 
-let nombre = param('--nombre')
-let apellido = param('--apellido')
+let name = param('--name')
+let lastname = param('--lastname')
+let age = param('--age')
 
-console.log( nombre )
-console.log( apellido )
+//console.log( nombre )
+//console.log( lastname )
+//console.log( age )
+
+console.log(`Your name is ${name} ${lastname} and you age is: ${age}`)
+
