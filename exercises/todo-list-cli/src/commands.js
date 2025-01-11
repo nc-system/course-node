abb6e661d5d7a554722d376906a698f85e479561
Andres Giraldo <andresganc@gmail.com>
2025-01-10 20:53:35 -0500

    Mod: Exercises: Todo List CLI: Commands

--- exercises/todo-list-cli/src/commands.js
+++ exercises/todo-list-cli/src/commands.js
@@ -3,12 +3,13 @@ const { Command } = require('commander');
 const program = new Command();
 
 program
-    .name('Todo List CLI')
-    .description('Todo List Memory - Command Line Interfaces')
-    .version('0.0.1');
+  .name('Todo List CLI')
+  .description('Todo List Memory - Command Line Interfaces')
+  .version('0.0.1');
 
-program.command('save').action(() => {
-    console.log('Saved');
+program.command('save <title> <description>').action((title, description) => {
+  console.log('title, description');
+  console.log('Saved');
 });
 
 program.parse(process.argv);
