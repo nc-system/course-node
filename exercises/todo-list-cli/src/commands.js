36afecab9a9c8885f06acbc99918b5e1efa77140
Andres Giraldo <andresganc@gmail.com>
2025-01-11 12:30:17 -0500

    Mod: Exercises: Todo List CLI: Commands

--- exercises/todo-list-cli/src/commands.js
+++ exercises/todo-list-cli/src/commands.js
@@ -1,5 +1,7 @@
 
-const { Command } = require('commander');
+import { Command } from 'commander'
+import inquirer from 'inquirer'
+
 const program = new Command();
 
 program
@@ -7,9 +9,37 @@ program
   .description('Todo List Memory - Command Line Interfaces')
   .version('0.0.1');
 
-program.command('save <title> <description>').action((title, description) => {
-  console.log('title, description');
-  console.log('Saved');
+program.command('save').action(async () => {
+
+  inquirer
+    .prompt([
+      /* Pass your questions in here */
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+      },
+      {
+        type: 'input',
+        name: 'username',
+        message: 'What is your username?'
+      }
+
+    ])
+    .then((answers) => {
+      // Use user feedback for... whatever!!
+    })
+    .catch((error) => {
+      if (error.isTtyError) {
+        // Prompt couldn't be rendered in the current environment
+      } else {
+        // Something else went wrong
+      }
+    });
+
+  // console.log('Response:', answers);
+  console.log('Successfully saved');
+
 });
 
 program.parse(process.argv);
